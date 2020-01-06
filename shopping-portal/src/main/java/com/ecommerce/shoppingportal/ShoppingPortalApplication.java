dd31d883e68e36ae3a82e3e4073975b07ad8a9f7
Catherine <xj573@nyu.edu>
2020-01-06 11:02:57 -0500

    upload eureka

--- shopping-portal/src/main/java/com/ecommerce/shoppingportal/ShoppingPortalApplication.java
+++ shopping-portal/src/main/java/com/ecommerce/shoppingportal/ShoppingPortalApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.shoppingportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ShoppingPortalApplication {
 
 	public static void main(String[] args) {
